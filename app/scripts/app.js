6238bc3481ab9bcfb8f9aec866a20a0096c8c162
aborche <ari.borchew@searshc.com>
2014-02-17 15:09:39 -0600

    Phonegap/Angular Scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('ctaApp', [
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
